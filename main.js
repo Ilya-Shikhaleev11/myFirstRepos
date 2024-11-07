8167291ed8c6ef10db22a646ee09bf183e23beeb
ilya shikhaleev <89536869140i@gmail.com>
2024-11-07 20:29:05 +0300

    add js

---
+++ main.js
@@ -0,0 +1,103 @@
+// console.log("Hello Js")
+
+//let message=number prompt("Ввыведите что-нибудь") 
+
+//console.log(message)
+
+//let number1 = Number(prompt("введите первое число")) 
+//let number2 = Number(prompt("введите второе число")) 
+//let rez = number1+number2
+//console.log("Результат:",rez)
+
+//let x = Number(prompt("введите число от 1 до 9"))
+//let x1=x*1
+//let x2=x*2
+//let x3=x*3
+//let x4=x*4
+//let x5=x*5
+//let x6=x*6
+//let x7=x*7
+//let x8=x*8
+//let x9=x*9
+//console.log(x+"*1"+ x1)
+//console.log(x+"*2"+ x2)
+//c//onsole.log(x+"*3"+ x3)
+//console.log(x+"*4"+ x4)
+//console.log(x+"*5"+ x5)
+//console.log(x+"*6"+ x6)
+//console.log(x+"*7"+ x7)
+//console.log(x+"*8"+ x8)
+//console.log(x+"*9"+ x9)
+
+//let answer = prompt("Введите да или нет")
+//if (answer === "Да")
+//{
+//    console.log("Согласие")
+//}
+//else
+//{
+//    console.log("Отрицание")
+//}
+
+// if (условие)
+//{
+//    тело_условия
+//}
+
+// Решить уровнение с 2 койфицентами ax+b=0
+//ax+b=0
+
+// let a = prompt("Введите коэфициент a ")
+// let b = prompt("Введите коэфициент b ")
+// if (a===0)
+// {
+//     console.log("Корней нет")
+// }
+// else
+// {
+//     console.log("x =",-b/a)
+// }
+
+// let a = prompt("Введите коэфициент a ")
+// let b = prompt("Введите коэфициент b ")
+// let c = prompt("Введите коэфициент c ")
+// let D = b(2**0.5)-4*a*c
+// if (D>0) 
+// {
+//     x1 = (-b + math.sqrt(D)) / (2 * a)
+//     x2 = (-b - math.sqrt(D)) / (2 * a)
+        
+// }
+// else (D<0)
+// {
+//     console.log("Корней нет")
+// }
+
+// for (let i=0; i<=5; i++)
+// { console.log("Иттерация"+i)}
+
+// let j = 0 
+// while (j>10)
+// {
+//     console.log("Иттерация " +J)
+//     j++
+// }
+
+// do
+// {console.log("while-Иттерация " +J)
+//     j--
+// }while (j>0)
+
+let x =prompt("введите число от 1 до 10") 
+for (let j=1; j<=10; j++){
+    for(let i=1; i<=10; i++)
+    
+        {
+                console.log(j+"*"+i+"="+j*i)
+        }
+        
+}
+
+
+
+
