01d702960b805ddb2c82d663d2b2f11539e77f43
ilya shikhaleev <89536869140i@gmail.com>
2024-11-14 18:59:49 +0300

    урок 3

---
+++ FIRSTVUE/vue-project/src/components/Calculator.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <h1>Калькулятор</h1>
+        <div>
+            {{ dispplayValue }}
+        </div>
+        <div>
+            <button v-for="btn in buttons" :key="btn" @click="handleButtonClick(btn)">
+                    {{ btn }} 
+            </button>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+export default defineComponent({
+    name:'Calculator',
+    setup(props, ctx) {
+        const dispplayValue = ref<string>('0')
+        const buttons = ref<string[]>([
+            '7','8','9','/',
+            '4','5','6','*',
+            '1','2','3','-',
+            '0','.','=','+',
+        ])
+        let currentInput = ref<string>('')
+        let operator = 
+    },)
