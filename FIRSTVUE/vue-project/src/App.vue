9076ad4adcc8ceee08d263f92ce187e2f04a6b0b
ilya shikhaleev <89536869140i@gmail.com>
2024-11-14 21:49:23 +0300

    урок 3 ввод данных файл

--- FIRSTVUE/vue-project/src/App.vue
+++ FIRSTVUE/vue-project/src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-
-
+import UserForm from './components/UserForm.vue';
 </script>
 
 <template>
+  <UserForm/>
   <RouterView />
 </template>
 
---
+++ FIRSTVUE/vue-project/src/components/UserForm.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <h1>форма обратной связи</h1>
+        <form @submit.prevent="submitForm">
+    
+            <div>
+                <label for="name">Имя:</label>
+                <input type="text" v-model="user.name" required />
+            </div>
+            <div>
+                <label for="email">Электронная почта:</label>
+                <input type="email" v-model="user.email" required />
+            </div>
+            <div>
+                <label for="age">Возраст:</label>
+                <input type="number" v-model="user.age" required />
+            </div>
+            <div>
+                <label for="text">Номер телефона:</label>
+                <input type="number" v-model="user.text" required />
+            </div>
+            <button tupe="submit">Подтвердить</button>
+        </form>
+        <div v-if="submitted">
+            <h2>Полученная информация:</h2>
+            <p>Иия: {{ user.name }}</p>
+            <p>Эл. почта:  {{ user.email }}</p>
+            <p>Возраст: {{ user.age }}</p>
+            <p>Номер телефона:{{ user.text }} </p>
+        </div>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+export default defineComponent(
+    {
+        name: "userform",
+        setup()
+        {
+            const user = ref
+            ({
+                name:'',
+                email: '',
+                age: null as number | null,
+                text:'',
+            });
+            const submitted = ref(false)
+            const submitForm = () =>
+            {
+                submitted.value =true;
+            };
+            return {
+                user,
+                submitted,
+                submitForm,
+            };
+        },       
+    }
+    );
+</script>
+
+
+
